8f3efb9f6596277abdac2d7cd77a3ec84f30c601
EmmaHu-learner <51519568+EmmaHu-learner@users.noreply.github.com>
2020-04-01 16:20:44 -0700

    UPDATE

--- app/run.py
+++ app/run.py
@@ -4,6 +4,9 @@ import pandas as pd
 
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
+from nltk import pos_tag
+from nltk.corpus import stopwords, wordnet
+import re
 
 from flask import Flask
 from flask import render_template, request, jsonify
@@ -11,16 +14,52 @@ from plotly.graph_objs import Bar, Histogram
 from sklearn.externals import joblib
 from sqlalchemy import create_engine
 
+from collections import Counter
+import operator
+import numpy as np
+
 app = Flask(__name__)
 
-def tokenize(text):
-    tokens = word_tokenize(text)
-    lemmatizer = WordNetLemmatizer()
+# def tokenize(text):
+#     tokens = word_tokenize(text)
+#     lemmatizer = WordNetLemmatizer()
+#
+#     clean_tokens = []
+#     for tok in tokens:
+#         clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+#         clean_tokens.append(clean_tok)
+#
+#     return clean_tokens
 
-    clean_tokens = []
-    for tok in tokens:
-        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
-        clean_tokens.append(clean_tok)
+def get_wordnet_pos(tag):
+    if tag.startswith('J'):
+        return wordnet.ADJ
+    elif tag.startswith('N'):
+        return wordnet.NOUN
+    elif tag.startswith('R'):
+        return wordnet.VERB
+    elif tag.startswith('V'):
+        return wordnet.ADV
+    else:
+        return None
+
+def tokenize(text):
+    # normalize text: Remove punctuation
+    text=re.sub(r"[^a-zA-Z0-9]", " ", text)
+
+    # tokenize and remove stop words
+    words=word_tokenize(text)
+    tokens=[w for w in words if w not in stopwords.words('english')]
+
+    # lemmatization depending on pos_tag
+    lemmatizer=WordNetLemmatizer()
+
+    clean_tokens=[]
+    for (tok,tag) in pos_tag(tokens):
+        # get pos_tag
+        wordnet_pos=get_wordnet_pos(tag) or wordnet.NOUN
+        # lemmatization
+        clean_tokens.append(lemmatizer.lemmatize(tok, pos=wordnet_pos).lower().strip())
 
     return clean_tokens
 
@@ -29,7 +68,7 @@ engine = create_engine('sqlite:///../data/DisasterResponse.db')
 df = pd.read_sql_table('message_categories', engine)
 
 # load model
-model = joblib.load("../models/DisasterResponseModel.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
@@ -39,11 +78,33 @@ def index():
     
     # extract data needed for visuals
     # TODO: Below is an example - modify to extract data for your own visuals
+
+    # for plot 1: genre and count
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
 
+    # for plot 2: category and count
     categories = df.columns[4:].tolist()
     received_msgs = df.iloc[:,4:].sum().tolist()
+
+    # for plot 3: most frequent words
+    words=[]
+    for text in df['message'].values:
+        # tokenized=
+        words.extend(tokenize(text))
+
+    word_counts=Counter(words)
+    sorted_word_counts=dict(sorted(word_counts.items(), key=operator.itemgetter(1), reverse=True))\
+
+    top=0
+    top_10={}
+    for i, j in sorted_word_counts.items():
+        top_10[i]=j
+        top+=1
+        if top==10:
+            break
+    top_words=list(top_10.keys())
+    top_counts=100*np.array(list(top_10.values()))/df.shape[0]
 
     # create visuals
     graphs = [
@@ -83,6 +144,25 @@ def index():
                 },
                 'xaxis': {
                     'title': "Categories"
+                }
+            }
+        },
+        {
+            'data': [
+                Bar(
+                    x=top_words,
+                    y=top_counts,
+                    name='Top_Words_Perc'
+                )
+            ],
+
+            'layout': {
+                'title': "Percentage of the 10 Most Frequent Words",
+                'yaxis': {
+                    'title': "Percentage of the 10 Most Frequent Words"
+                },
+                'xaxis': {
+                    'title': "Top 10 Words"
                 }
             }
         }
